e8dfaa0b315420b95e0980f3ad4aafb1a0c3b685
anirudhi89 <anirudh.iyer@outlook.com>
2023-08-20 03:28:29 -0700

    spring initializer

---
+++ src/main/java/com/NetflixCommentSectionServer/NetflixCommentSectionServer/NetflixCommentSectionServerApplication.java
@@ -0,0 +1,13 @@
+package com.NetflixCommentSectionServer.NetflixCommentSectionServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixCommentSectionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixCommentSectionServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NetflixCommentSectionServer/NetflixCommentSectionServer/NetflixCommentSectionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NetflixCommentSectionServer.NetflixCommentSectionServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixCommentSectionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
